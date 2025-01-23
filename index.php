5d428de87e1fcb5ca56bb02f092f25c202000dc6
RicardoMacagba <155034677+RicardoMacagba@users.noreply.github.com>
2025-01-23 15:31:39 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Portfolio Website
+    </title>
+</head>
+<body>
+    <h1>Name: Ricardo T. Macagba Jr.</h1>
+    <h1>Role: Backend Developer</h1>
+    <h1>About Me: </h1>
+</body>
+</html>
