35d15073b6fb21f7da63e53b3ca1edb22dc70da5
RicardoMacagba <155034677+RicardoMacagba@users.noreply.github.com>
2025-01-23 15:50:43 +0800

    Initial Commit

--- index.php
+++ index.php
@@ -1,14 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Portfolio Website
     </title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        div {
+            margin: 0 auto;
+            width: 50%;
+            padding: 20px;
+            background-color: #fff;
+            margin-top: 50px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+        }
+        footer {
+            text-align: center;
+            margin-top: 50px;
+            padding: 10px;
+            background-color: cadetblue;
+            color: #fff;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Name: Ricardo T. Macagba Jr.</h1>
-    <h1>Role: Backend Developer</h1>
-    <h1>About Me: </h1>
+    <div>
+        <h1>Simple Portfolio Website</h1>
+        <h1>Name: Ricardo T. Macagba Jr.</h1>
+        <h1>Role: Backend Developer</h1>
+        <h1>About Me: Always want to learn something. I also need to learn how to ask to learn more effectively. <br><br>
+            I want to learn backend development. I am also familiar with basic front-end technologies like HTML, CSS, and JavaScript. <br><br>
+            I am a quick learner and always eager to learn new technologies. <br><br>
+            I am looking for a challenging role in a reputable organization to utilize my skills and grow as a professional.
+        </h1>
+    </div>
+<footer>
+    <h1>
+        &copy; 2025 Simple Portfolio Website
+    </h1>
+</footer>
 </body>
+
 </html>
